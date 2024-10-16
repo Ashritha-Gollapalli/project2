029dd67a697b0863342423bc38e3f47195aa2950
Ashritha-Gollapalli <ashritha.g2004@gmail.com>
2024-10-16 10:26:12 +0530

    reactjs

---
+++ test1/src/App.jsx
@@ -0,0 +1,18 @@
+import {useState} from 'react'
+
+const App = () => {
+  const [count,setCount]=useState(0);
+  return (
+    <div>
+      <center>
+        <h1>
+          {count}
+        </h1>
+        <br/>
+        <button onClick={()=>setCount(count+1)}>useState</button>
+      </center>
+    </div>
+  )
+}
+
+export default App
